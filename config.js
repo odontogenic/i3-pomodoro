2dcdb9c661487ce1c6bc2ce882f92bd374f73d71
saliya seneviratne <sseneviratne@thinkful.com>
2015-11-16 11:30:44 -0500

    initialize from config file. document this

--- config.js
+++ config.js
@@ -2,5 +2,10 @@
 
 export default ({
     hostname: 'localhost',
-    port: 41234
+    port: 41234,
+    periods: { // note: these values are only used once! use `pomodoro set <....>'
+        session: 1000 * 60 * 25,
+        shortBreak: 1000 * 60 * 5,
+        longBreak: 1000 * 60 * 15
+    }
 })
