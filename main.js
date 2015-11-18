2b6f6890897a301b95210803c7e865bf15e942ea
saliya seneviratne <sseneviratne@thinkful.com>
2015-11-17 20:52:45 -0500

    instantiate pomodoro from main for proper cleanup

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 'use strict'
 
 
-import config from './config'
-import server from './lib/server.js'
+import Server from './lib/server'
+import Pomodoro from './lib/pomodoro'
 
-server.bind(config.port, () => {
-    console.log(`Server bound on port ${config.port}`)
-})
+let pomodoro = new Pomodoro(),
+    server = new Server(pomodoro)
+
+server.run()
 
 function exitHandler(err, options) {
     if (err) console.log(err.stack)
 
-    if (options.cleanup) console.log(`options.cleanup`)
+    pomodoro.writer.close()
+    pomodoro.store.close()
 
     if (options.exit) {
         process.exit(err ? 1 : 0)
@@ -20,10 +22,14 @@ function exitHandler(err, options) {
 
 
 // do something when app is closing
-process.on('exit', exitHandler.bind(null, null, {cleanup:true}))
+process.on('exit', exitHandler.bind(null, null, {
+    cleanup: true
+}))
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, null, {exit:true}))
+process.on('SIGINT', exitHandler.bind(null, null, {
+    exit: true
+}))
 
 //catches uncaught exceptions
 process.on('uncaughtException', exitHandler.bind(null))
