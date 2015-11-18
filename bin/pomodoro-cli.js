d89a058a710f3964f80571b57e97bfd8032dc137
saliya seneviratne <sseneviratne@thinkful.com>
2015-11-18 11:53:53 -0500

    clarify options and save redis on exit

--- bin/pomodoro-cli.js
+++ bin/pomodoro-cli.js
@@ -6,12 +6,11 @@
  * USAGE
  *
  * pomodoro <>
- *   --toggle
  *   --pause
  *   --resume
- *   --stop
- *   --reset
- *
+ *   --toggle
+ *   --clear
+ *   --next
  */
 
 'use strict'
@@ -24,13 +23,11 @@ import ActionTypes from '../lib/actions'
 
 program
     .version('0.0.1')
-    .option('-t, --toggle', 'Toggle session')
-    .option('-R, --reset', 'Reset current session')
     .option('-p, --pause', 'Pause session')
     .option('-r, --resume', 'Resume session')
-    .option('-n, --next', 'Skip to next session')
+    .option('-t, --toggle', 'Toggle session')
     .option('-c, --clear', 'Clear sessions')
-    // .option('-s, --set <config>', 'Set config', list)
+    .option('-n, --next', 'Next session')
     .parse(process.argv)
 
 if (program.toggle) {
@@ -39,8 +36,6 @@ if (program.toggle) {
     send(ActionTypes.PAUSE)
 } else if (program.resume) {
     send(ActionTypes.RESUME)
-} else if (program.reset) {
-    send(ActionTypes.RESET)
 } else if (program.next) {
     send(ActionTypes.NEXT)
 } else if (program.clear) {
@@ -49,21 +44,6 @@ if (program.toggle) {
     program.outputHelp()
     process.exit(1)
 }
-
-// function list(val) {
-//     return val.split(',')
-// }
-//
-// function set(config) {
-//
-//     let time = {
-//         session: 1000 * 60 * 25,
-//         shortBreak: 1000 * 60 * 5,
-//         longBreak: 1000 * 60 * 15
-//     }
-//
-//     send(ActionTypes.RESET)
-// }
 
 function send(action) {
     let client = dgram.createSocket("udp4")
--- main.js
+++ main.js
@@ -12,7 +12,7 @@ server.run()
 function exitHandler(err, options) {
     if (err) console.log(err.stack)
 
-    pomodoro.write() // write final state
+    pomodoro.pause()
     pomodoro.writer.close()
     pomodoro.store.close()
 
@@ -23,14 +23,10 @@ function exitHandler(err, options) {
 
 
 // do something when app is closing
-process.on('exit', exitHandler.bind(null, null, {
-    cleanup: true
-}))
+process.on('exit', exitHandler.bind(null, null, { cleanup: true }))
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, null, {
-    exit: true
-}))
+process.on('SIGINT', exitHandler.bind(null, null, { exit: true }))
 
 //catches uncaught exceptions
 process.on('uncaughtException', exitHandler.bind(null))
